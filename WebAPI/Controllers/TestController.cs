9f21e265e755d7596184b78d146ba9a20f2d3396
david <davidhs97@outlook.com>
2022-03-26 13:37:13 +0700

    Add project files.

---
+++ WebAPI/Controllers/TestController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System;
+using System.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public TestController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            string query = @"select * from Test01 where id = @id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MariaDb");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand=new MySqlCommand(query, mycon))
+                {
+                    mycommand.Parameters.AddWithValue("@id", id);
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            string query = @"select * from Test01";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MariaDb");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost("create")]
+
+        public IActionResult Create(Test01 model)
+        {
+            string query = @"insert into Test01 (Nama, Status, Created, Updated)
+                VALUES (@nama, @status, @created, @updated)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MariaDb");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    mycommand.Parameters.AddWithValue("@nama", model.Nama);
+                    mycommand.Parameters.AddWithValue("@status", model.Status);
+                    mycommand.Parameters.AddWithValue("@created", DateTime.Now);
+                    mycommand.Parameters.AddWithValue("@updated", DateTime.Now);
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut("update")]
+
+        public IActionResult Update(Test01 model)
+        {
+            string query = @"update Test01 
+                set Nama = @nama, Updated = @updated 
+                where id = @id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MariaDb");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    mycommand.Parameters.AddWithValue("@nama", model.Nama);
+                    mycommand.Parameters.AddWithValue("@id", model.Id);
+                    mycommand.Parameters.AddWithValue("@updated", DateTime.Now);
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Updated successfully");
+        }
+
+        [HttpDelete("delete/{id}")]
+
+        public JsonResult Delete(int id)
+        {
+            string query = @"delete from Test01 where id = @id";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = configuration.GetConnectionString("MariaDb");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand mycommand = new MySqlCommand(query, mycon))
+                {
+                    mycommand.Parameters.AddWithValue("@id", id);
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ WebAPI/Models/Test01.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebAPI.Models
+{
+    public class Test01
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public int Status { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
